93b1a3489c548085db6c9168c07c6af328a4dbbb
Hasemann <stefanhasemann1@gmail.com>
2025-07-17 22:20:31 +0200

    Refactoring vue component to handle js data instead of twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\ApiResource\OsrsApiService;
+use Exception;
 use JsonException;
 use Saloon\Exceptions\Request\FatalRequestException;
 use Saloon\Exceptions\Request\RequestException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -18,14 +21,32 @@ class HomeController extends AbstractController
     }
 
     /**
-     * @throws FatalRequestException
-     * @throws RequestException
-     * @throws JsonException
+     * @return Response
      */
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        $playerStats = $this->osrsApiService->getFormattedPlayerStats('brokedevelop');
-        return $this->render('/base.html.twig', ['playerStats' => $playerStats]);
+        return $this->render('/base.html.twig');
+    }
+
+    /**
+     * @throws FatalRequestException
+     * @throws RequestException
+     * @throws JsonException
+     */
+    #[Route(
+        '/api/osrs/{username}',
+        name: 'api_osrs_stats',
+        methods: ['GET']
+    )]
+    public function stats(string $username): JsonResponse
+    {
+        try {
+            $playerStats = $this->osrsApiService->getFormattedPlayerStats($username);
+        }catch (Exception $exception){
+            return new JsonResponse(['errorMessage' => 'user not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->json($playerStats);
     }
 }
