94a53f23b7eca9d1b99da57bb54158123b7c165e
Hasemann <stefanhasemann1@gmail.com>
2025-07-19 22:31:09 +0200

    Refactoring main app component, to handle progress bar and toggle buttons for grid layout

--- assets/vue/App.vue
+++ assets/vue/App.vue
@@ -3,29 +3,49 @@
         <h1 class="p-2 pl-1.5">
             Stats view from OldSchool Runescape
         </h1>
+        <div class="flex flex-row justify-between">
+            <div>
+                <div v-if="error" v-text="error" class="text-red-600 p-2"></div>
+                <input
+                    class="px-2 py-1 border"
+                    :class="{'border-green-600': !error}"
+                    type="text"
+                    v-model="playerName"
+                    :value="brokedeveloper"
+                    placeholder="Enter OSRS Username"
+                />
+                <button class="btn bg-indigo-600 text-white px-3 py-1" @click="fetchStats">Fetch</button>
+            </div>
+            <grid-buttons @updateGrid="toggleGrid"></grid-buttons>
+        </div>
 
-        <div v-if="error" v-text="error" class="text-red-600 p-2"></div>
-        <input
-            class="px-2 py-1 border rounded"
-            :class="{'border-green-600': !error}"
-            type="text"
-            v-model="playerName"
-            placeholder="Enter OSRS Username"
-        />
-        <button class="btn bg-indigo-600 text-white px-3 py-1 rounded" @click="fetchStats">Fetch</button>
-        <div class="grid md:grid-cols-4 gap-2 grid-cols-2 sm:grid-cols-3">
+        <div class="grid gap-2 grid-cols-2 mt-2"
+             :class="gridClass">
+
             <template v-for="(stats, index) in statsData" :key="index">
                 <div
                     class="flex p-3 sm:p-5 gap-2 bg-white flex-col items-center border border-indigo-500/50 justify-center">
                     <div class="uppercase text-2xl" v-text="index"></div>
-                    <div v-text="stats.level"></div>
-                    /99
-                    XP:
-                    <div v-text="stats.xp"></div>
-                    <div class="italic">to next level(d):
-                        <br/>
-                        <div v-text="stats.xpToNext"></div>
+                    <div class="flex flex-row">
+                        <div v-text="stats.level"></div>
+                        <template v-if="index !== 'overall'">
+                            <div>/99</div>
+                        </template>
                     </div>
+                    <div class="flex flex-row">
+                        <div>XP:</div>
+                        <div v-text="stats.xp"></div>
+                    </div>
+                    <template v-if="index !== 'overall'">
+                        <div class="flex flex-row text-xs italic">to next level(
+                            <div v-text="stats.level + 1"></div>
+                            )
+                            <br/>
+                            <div v-text="stats.totalXpToNext - stats.xp"></div>
+                        </div>
+                        <progress :value="progressPercentage(stats)" max="100"></progress>
+
+                    </template>
                 </div>
             </template>
         </div>
@@ -33,21 +53,39 @@
 </template>
 
 <script>
+import GridButtons from "./GridButtons.vue";
+
 export default {
     name: 'OsrsStats',
+    components: {GridButtons},
     data() {
         return {
+            grid: 4,
             statsData: null,
-            selectedSkill: 'strength',
-            playerName: '',
+            exp: 0,
+            expToNext: 0,
+            progress: 0,
+            playerName: 'brokedevelop',
             isLoading: false,
             error: null
         };
+    },
+    computed: {
+        gridClass() {
+            return {
+                'md:grid-cols-3': this.grid === 3,
+                'md:grid-cols-4': this.grid === 4,
+            }
+        }
     },
     mounted() {
 
     },
     methods: {
+        toggleGrid(gridInput) {
+            this.grid = gridInput
+        },
+
         async fetchStats() {
             try {
                 if (!this.playerName || this.playerName.trim() === '') {
@@ -73,24 +111,15 @@ export default {
         setPlayerName(name) {
             this.playerName = name;
         },
-        progressPercentage() {
-            if (!this.statsData || !this.statsData?.[this.selectedSkill]) {
-                return 0;
-            }
-            const {xp, level, xpToNext} = this.statsData?.[this.selectedSkill];
-            if (level >= 99) {
+        progressPercentage(stats) {
+            if (!stats || stats.level >= 99) {
                 return 100;
             }
-            const xpThisLevel = xp - xpToNext;
-            const xpForNext = this.xpForLevel(level + 1) - this.xpForLevel(level);
-            return (xpThisLevel / xpForNext) * 100;
-        },
-        xpForLevel(level) {
-            let total = 0;
-            for (let i = 1; i < level; i++) {
-                total += Math.floor(i + 300 * Math.pow(2, i / 7)) / 4;
-            }
-            return Math.floor(total);
+            const xpTowardsNext = stats.xp - stats.startingXp;
+            const xpNeededForNext = stats.totalXpToNext - stats.startingXp;
+            const progress = (xpTowardsNext / xpNeededForNext) * 100;
+
+            return Math.min(100, Math.max(0, progress));
         },
     },
 };
